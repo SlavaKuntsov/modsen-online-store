0e833d8c82fb467c75ab891242b67eb346d7a2d1
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-06-24 18:29:23 +0300

    feat: ef core configuration

---
+++ src/server/OnlineStore.Infrastructure/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Infrastructure.DataAccess;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+	public DbSet<User> Users { get; set; }
+	public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		base.OnModelCreating(modelBuilder);
+	}
+}
---
+++ src/server/OnlineStore.Infrastructure/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OnlineStore.Infrastructure.DataAccess;
+
+namespace OnlineStore.Infrastructure.Extensions;
+
+public static class InfrastructureExtensions
+{
+	public static IServiceCollection AddInfrastructure(
+		this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") 
+								?? configuration.GetConnectionString(nameof(ApplicationDbContext));
+
+		services.AddDbContextPool<ApplicationDbContext>(
+			options =>
+			{
+				options.UseNpgsql(connectionString);
+			},
+			128);
+		
+		return services;
+	}
+	
+	public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+	{
+		using var scope = app.ApplicationServices.CreateScope();
+		var services = scope.ServiceProvider;
+		var logger = services.GetRequiredService<ILogger<ApplicationDbContext>>();
+
+		try
+		{
+			var context = services.GetRequiredService<ApplicationDbContext>();
+			context.Database.Migrate();
+			logger.LogInformation("Database migrations applied successfully");
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "An error occurred while applying database migrations");
+			throw;
+		}
+
+		return app;
+	}
+}
