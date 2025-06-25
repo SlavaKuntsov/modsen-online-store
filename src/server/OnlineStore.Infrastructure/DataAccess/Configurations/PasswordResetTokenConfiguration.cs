3a39ef1bd56bc70367efe403638f35ce80f61203
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-06-25 23:03:32 +0300

    feat: reset password entity

---
+++ src/server/OnlineStore.Infrastructure/DataAccess/Configurations/PasswordResetTokenConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Infrastructure.DataAccess.Configurations;
+
+
+public class PasswordResetTokenConfiguration : IEntityTypeConfiguration<PasswordResetToken>
+{
+	public void Configure(EntityTypeBuilder<PasswordResetToken> builder)
+	{
+		builder.HasKey(r => r.Id);
+
+		builder.Property(r => r.Id)
+			.IsRequired();
+
+		builder.Property(r => r.UserId)
+			.IsRequired();
+
+		builder.Property(r => r.Token)
+			.IsRequired()
+			.HasMaxLength(500);
+
+		builder.Property(r => r.ExpiresAt)
+			.IsRequired();
+
+		builder.Property(r => r.CreatedAt)
+			.IsRequired();
+
+		builder.Property(r => r.IsUsed)
+			.IsRequired();
+		
+		builder.HasOne(t => t.User)
+			.WithMany() 
+			.HasForeignKey(t => t.UserId)
+			.OnDelete(DeleteBehavior.Cascade);
+	}
+}
