431f72549b76a24c7c893b0a5a0956c8c8e9c876
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-08-20 21:46:12 +0300

    Handle empty parent category id

--- src/server/OnlineStore.Application/Categories/CreateCategoryCommand.cs
+++ src/server/OnlineStore.Application/Categories/CreateCategoryCommand.cs
@@ -1,15 +1,22 @@
-﻿using MediatR;
+using MediatR;
 using OnlineStore.Application.Abstractions.Data;
+using OnlineStore.Application.Dtos;
+using OnlineStore.Domain.Entities;
 
 namespace OnlineStore.Application.Categories;
 
-public record CreateCategoryCommand() : IRequest;
+public sealed record CreateCategoryCommand(string Name, Guid? ParentCategoryId = null) : IRequest<CategoryDto>;
 
 public sealed class CreateCategoryCommandHandler(IApplicationDbContext dbContext)
-	: IRequestHandler<CreateCategoryCommand>
+        : IRequestHandler<CreateCategoryCommand, CategoryDto>
 {
-	public Task Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
-	{
-		throw new NotImplementedException();
-	}
+        public async Task<CategoryDto> Handle(CreateCategoryCommand request, CancellationToken ct)
+        {
+                var category = new Category(request.Name, request.ParentCategoryId);
+
+                await dbContext.Categories.AddAsync(category, ct);
+                await dbContext.SaveChangesAsync(ct);
+
+                return new CategoryDto(category.Id, category.Name, category.ParentCategoryId, new List<CategoryDto>());
+        }
 }
