a6e00d41cee64f39181e7c112971f0b23e909458
Кунцов Святослав <75099655+SlavaKuntsov@users.noreply.github.com>
2025-08-21 00:51:00 +0300

    ref

--- src/server/OnlineStore.Persistance/DataAccess/Configurations/CartConfiguration.cs
+++ src/server/OnlineStore.Persistance/DataAccess/Configurations/CartConfiguration.cs
@@ -6,23 +6,23 @@ namespace OnlineStore.Persistance.DataAccess.Configurations;
 
 public class CartConfiguration : IEntityTypeConfiguration<Cart>
 {
-        public void Configure(EntityTypeBuilder<Cart> builder)
-        {
-                builder.HasKey(c => c.Id);
+	public void Configure(EntityTypeBuilder<Cart> builder)
+	{
+		builder.HasKey(c => c.Id);
 
-                builder.OwnsMany(c => c.Items, itemBuilder =>
-                {
-                        itemBuilder.WithOwner().HasForeignKey("CartId");
-                        itemBuilder.HasKey("CartId", nameof(CartItem.ProductId));
+		builder.OwnsMany(c => c.Items, itemBuilder =>
+		{
+			itemBuilder.WithOwner().HasForeignKey("CartId");
+			itemBuilder.HasKey("CartId", nameof(CartItem.ProductId));
 
-                        itemBuilder.Property(i => i.ProductId).IsRequired();
-                        itemBuilder.Property(i => i.ProductName).IsRequired();
-                        itemBuilder.Property(i => i.UnitPrice).HasColumnType("decimal(18,2)");
-                        itemBuilder.Property(i => i.Quantity).IsRequired();
+			itemBuilder.Property(i => i.ProductId).IsRequired();
+			itemBuilder.Property(i => i.ProductName).IsRequired();
+			itemBuilder.Property(i => i.UnitPrice).HasColumnType("decimal(18,2)");
+			itemBuilder.Property(i => i.Quantity).IsRequired();
 
-                        itemBuilder.ToTable("CartItems");
-                });
+			itemBuilder.ToTable("CartItems");
+		});
 
-                builder.ToTable("Carts");
-        }
+		builder.ToTable("Carts");
+	}
 }
